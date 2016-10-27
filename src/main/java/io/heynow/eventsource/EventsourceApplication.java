e0d698be979af15a9fffc8a60d2544fe280f9906
Jakub Wrabel <jakub.wrabel@ocado.com>
2016-10-27 14:37:59 +0200

    Spring cloud integration

--- src/main/java/io/heynow/eventsource/EventsourceApplication.java
+++ src/main/java/io/heynow/eventsource/EventsourceApplication.java
@@ -1,9 +1,10 @@
 package io.heynow.eventsource;
 
+import io.heynow.stream.manager.client.EnableStreamManagerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableAutoConfiguration
 @Configuration
-@EnableFeignClients
+@EnableStreamManagerClient
+@EnableEurekaClient
 public class EventsourceApplication {
 
     public static void main(String[] args) {
